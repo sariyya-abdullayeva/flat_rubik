ae9ad808dcd88a242c3c47b040c7d8098fc8c07e
Sariyya Abdullayeva <sariyyaabdullayeva@Sariyyas-Air.lan>
2020-09-29 21:56:54 +0300

    Update: rotateCV() Add:updateColors()

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ class Game {
     constructor (faces) {
         this.faces = faces;
     }
-      // Parameters
+    // Parameters
     faceInPosition(position) {
         let facesInPosition = this.faces.filter(face => face.number == position);
         return facesInPosition[0];
@@ -15,38 +15,44 @@ class Face {
         this.blocks = blocks;
         this.sides = sides;
     }
-
+    
     blockInPosition(position) {
         let blocksInPosition = this.blocks.filter(block => block.position == position);
         return blocksInPosition[0];
     }
-
+    
     rotateCV() {
-        let prevBlocks = { ...this.blocks }; 
-
-        console.log("this.blocks is :");
-        console.log(this.blocks);
-
+        let prevBlockColors = this.blocks.map((block) => block.color); 
+        
         console.log("this is prevBlocks:");
-        console.log(prevBlocks);
-
-        // this.blockInPosition(2).color = prevFaceState.blockInPosition(4).color;
-        // this.blockInPosition(1).color = prevFaceState.blockInPosition(7).color;
-        // this.blockInPosition(3).color = prevFaceState.blockInPosition(1).color;
-        // this.blockInPosition(4).color = prevFaceState.blockInPosition(8).color;
-        // this.blockInPosition(6).color = prevFaceState.blockInPosition(2).color;
-        // this.blockInPosition(7).color = prevFaceState.blockInPosition(9).color;
-        // this.blockInPosition(8).color = prevFaceState.blockInPosition(6).color;
-        // this.blockInPosition(9).color = prevFaceState.blockInPosition(3).color;
-
-        this.blockInPosition(3).color = "temp_color_new"
-
-        console.log("this.blocks is :");
-        console.log(this.blocks);
-
-        console.log("this is prevBlocks:");
-        console.log(prevBlocks);
-
+        console.log(prevBlockColors);
+        
+        this.blockInPosition(2).color = prevBlockColors[3]
+        this.blockInPosition(1).color = prevBlockColors[6]
+        this.blockInPosition(3).color = prevBlockColors[0]
+        this.blockInPosition(4).color = prevBlockColors[7]
+        this.blockInPosition(6).color = prevBlockColors[1]
+        this.blockInPosition(7).color = prevBlockColors[8]
+        this.blockInPosition(8).color = prevBlockColors[5]
+        this.blockInPosition(9).color = prevBlockColors[2]
+        
+        // (4) 3
+        // (7) 6
+        // (1) 0
+        // (8) 7
+        // (2) 1
+        // (9) 8
+        // (6) 5
+        // (3) 2
+        
+        // this.blockInPosition(3).color = prevBlocks[0]
+        
+        // console.log("this.blocks is :");
+        // console.log(this.blocks);
+        
+        // console.log("this is prevBlocks:");
+        // console.log(prevBlocks);
+        
     }
 }
 
@@ -63,7 +69,7 @@ class TripBlock{
     }
 }
 
-let colors = ["green", "yellow", "white", "blue", "red", "oreange"]
+let colors = ["green", "yellow", "white", "blue", "red", "orange"]
 
 let faces = []
 for (let faceIndex = 1; faceIndex <= colors.length; faceIndex++) {
@@ -80,15 +86,25 @@ for (let faceIndex = 1; faceIndex <= colors.length; faceIndex++) {
 
 let game = new Game(faces);
 console.log(game)
+let faceDivs = document.querySelectorAll(".face");
 
-let faceDivs = document.querySelectorAll(".face")
+// for (var i = 0; i < faceDivsChild.length; i++) {
+//     faceDivsChild[i].style.color = colors[i];  //do styling here
+// }
+
+updateColors();
 faceDivs.forEach((faceDiv, index) => {
+    faceDiv.setAttribute("id", index + 1)
+    faceDiv.addEventListener("click", function(e){
+        game.faceInPosition(index + 1).rotateCV();
+    });
+});
+
+
+function updateColors() {
+    faceDivs.forEach((faceDiv, index) => {
         let faceNumber = index + 1;
-        faceDiv.setAttribute("id", faceNumber)
-        faceDiv.addEventListener("click", function(){
-            game.faceInPosition(faceNumber).rotateCV();
-        });
-    }
-);
-
-
+        let faceDivBlocks= [...faceDiv.children]
+        faceDivBlocks.forEach((block, blockIndex)=> block.classList.add(game.faceInPosition(faceNumber).blockInPosition(blockIndex+1).color))
+    });
+}
