c74e220610d450fa7f4509211e694b74b9dab265
sariyya-abdullayeva <sariyya.abdullayeva@gmail.com>
2021-05-07 09:55:34 +0300

    edit: shuffle, back buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Game {
-    constructor (faces) {
+    constructor(faces) {
         this.faces = faces;
         this.stepHistory = [];
     }
@@ -19,7 +19,7 @@ class Game {
 
     stepBack() {
         let lastStep = this.stepHistory.pop();
-        if (lastStep.isRotateCV){
+        if (lastStep.isRotateCV) {
             this.faceInPosition(lastStep.facePos).rotateCounterCV()
         } else {
             this.faceInPosition(lastStep.facePos).rotateCV()
@@ -29,7 +29,7 @@ class Game {
     solve() {
         while (this.stepHistory.length > 0) {
             let lastStep = this.stepHistory.pop();
-            if (lastStep.isRotateCV){
+            if (lastStep.isRotateCV) {
                 this.faceInPosition(lastStep.facePos).rotateCounterCV()
             } else {
                 this.faceInPosition(lastStep.facePos).rotateCV()
@@ -39,20 +39,20 @@ class Game {
 }
 
 class Face {
-    constructor (number, blocks, sides) {
+    constructor(number, blocks, sides) {
         this.number = number;
         this.blocks = blocks;
         this.sides = sides;
     }
-    
+
     blockInPosition(position) {
         let blocksInPosition = this.blocks.filter(block => block.position == position);
         return blocksInPosition[0];
     }
-    
+
     rotateCV() {
-        let prevBlockColors = this.blocks.map((block) => block.color); 
-        
+        let prevBlockColors = this.blocks.map((block) => block.color);
+
         this.blockInPosition(2).color = prevBlockColors[3] //4
         this.blockInPosition(1).color = prevBlockColors[6] //5
         this.blockInPosition(3).color = prevBlockColors[0] //1
@@ -61,23 +61,23 @@ class Face {
         this.blockInPosition(7).color = prevBlockColors[8] //9
         this.blockInPosition(8).color = prevBlockColors[5] //6
         this.blockInPosition(9).color = prevBlockColors[2] //3
-        
+
         let tempLastSideColors = this.sides[3].blocks.map(block => block.color)
-        for (let i = this.sides.length-1; i > 0; i--) {
+        for (let i = this.sides.length - 1; i > 0; i--) {
             this.sides[i].blocks.forEach((block, blockIndex) => {
-                block.color = this.sides[i-1].blocks[blockIndex].color 
+                block.color = this.sides[i - 1].blocks[blockIndex].color
             });
         }
         this.sides[0].blocks.forEach((block, blockIndex) => {
             block.color = tempLastSideColors[blockIndex]
         });
-        
+
         updateColors();
     }
 
     rotateCounterCV() {
-        let prevBlockColors = this.blocks.map((block) => block.color); 
-        
+        let prevBlockColors = this.blocks.map((block) => block.color);
+
         this.blockInPosition(2).color = prevBlockColors[5] //6
         this.blockInPosition(1).color = prevBlockColors[2] //3
         this.blockInPosition(3).color = prevBlockColors[8] //9
@@ -86,11 +86,11 @@ class Face {
         this.blockInPosition(7).color = prevBlockColors[0] //1
         this.blockInPosition(8).color = prevBlockColors[3] //4
         this.blockInPosition(9).color = prevBlockColors[6] //7
-        
+
         let tempFirstSideColors = this.sides[0].blocks.map(block => block.color)
-        for (let i = 0; i < this.sides.length-1; i++) {
+        for (let i = 0; i < this.sides.length - 1; i++) {
             this.sides[i].blocks.forEach((block, blockIndex) => {
-                block.color = this.sides[i+1].blocks[blockIndex].color 
+                block.color = this.sides[i + 1].blocks[blockIndex].color
             });
         }
         this.sides[3].blocks.forEach((block, blockIndex) => {
@@ -108,15 +108,15 @@ class Block {
     }
 }
 
-class TripBlock{
-    constructor(facePos, blocks){
+class TripBlock {
+    constructor(facePos, blocks) {
         this.facePos = facePos;
         this.blocks = blocks;
     }
 }
 
 class Step {
-    constructor (facePos, isRotateCV) {
+    constructor(facePos, isRotateCV) {
         this.facePos = facePos;
         this.isRotateCV = isRotateCV;
     }
@@ -129,10 +129,10 @@ for (let faceIndex = 1; faceIndex <= colors.length; faceIndex++) {
     const element = colors[faceIndex];
     let blocks = []
     for (let blockIndex = 1; blockIndex <= 9; blockIndex++) {
-        let block = new Block(colors[faceIndex-1], blockIndex)
+        let block = new Block(colors[faceIndex - 1], blockIndex)
         blocks.push(block)
     }
-    
+
     let face = new Face(faceIndex, blocks, undefined)
     faces.push(face);
 }
@@ -146,7 +146,7 @@ updateColors();
 // Add eventListeners
 faceDivs.forEach((faceDiv, index) => {
     faceDiv.setAttribute("id", index + 1)
-    faceDiv.addEventListener("click", function(e){
+    faceDiv.addEventListener("click", function (e) {
         if (e.shiftKey) {
             game.faceInPosition(index + 1).rotateCounterCV();
             game.stepHistory.push(new Step(index + 1, false));
@@ -154,7 +154,7 @@ faceDivs.forEach((faceDiv, index) => {
             game.faceInPosition(index + 1).rotateCV();
             game.stepHistory.push(new Step(index + 1, true));
         }
-        
+
     });
 });
 
@@ -166,11 +166,11 @@ faceDivs.forEach((faceDiv, index) => {
 function updateColors() {
     faceDivs.forEach((faceDiv, index) => {
         let faceNumber = index + 1;
-        let faceDivBlocks= [...faceDiv.children]
-        faceDivBlocks.forEach((block, blockIndex)=> {
+        let faceDivBlocks = [...faceDiv.children]
+        faceDivBlocks.forEach((block, blockIndex) => {
             //Re
             block.className = '';
-            block.classList.add('block',game.faceInPosition(faceNumber).blockInPosition(blockIndex+1).color)
+            block.classList.add('block', game.faceInPosition(faceNumber).blockInPosition(blockIndex + 1).color)
         })
     });
 }
@@ -196,7 +196,7 @@ const sidesObj = {
         4: [9, 6, 3],
         2: [9, 6, 3],
     },
-    4:{
+    4: {
         2: [7, 8, 9],
         3: [7, 8, 9],
         5: [3, 2, 1],
@@ -214,95 +214,98 @@ const sidesObj = {
         2: [3, 2, 1],
         1: [3, 2, 1]
     }
-    
+
 }
 
 // add sides to the each face
-for (let facePos in sidesObj){
+for (let facePos in sidesObj) {
     let tempSides = [];
-    for (let sideFacePosition in sidesObj[facePos]){
+    for (let sideFacePosition in sidesObj[facePos]) {
 
-        let tempBlocksArray = sidesObj[facePos][sideFacePosition].map(function (blockNumber){
+        let tempBlocksArray = sidesObj[facePos][sideFacePosition].map(function (blockNumber) {
             return game.faceInPosition(sideFacePosition).blockInPosition(blockNumber)
         });
-        
+
         let tripBlock = new TripBlock(sideFacePosition, tempBlocksArray);
         tempSides.push(tripBlock);
     }
     game.faceInPosition(facePos).sides = tempSides;
 }
-
-let buttonShuffle = document.querySelector('button')
-
-buttonShuffle.addEventListener("click", function(e){
+// shuffle button
+let buttonShuffle = document.querySelector('.shuffle');
+buttonShuffle.addEventListener("click", function (e) {
     game.shuffle(3)
-
 });
 
+// //back button
+// let buttonBack = document.querySelector('.back');
+// buttonBack.addEventListener("click", function (e) {
+//     game.stepBack()
+// });
 
 
 
 // this a temporary code for prototype testing. TODO: remove later
 {
-// game.faceInPosition(1).blockInPosition(1).color = 'white'
-// game.faceInPosition(1).blockInPosition(2).color = 'blue'
-// game.faceInPosition(1).blockInPosition(3).color = "yellow"
-// game.faceInPosition(1).blockInPosition(4).color = "green"
-// game.faceInPosition(1).blockInPosition(5).color = "red"
-// game.faceInPosition(1).blockInPosition(6).color = "orange"
-// game.faceInPosition(1).blockInPosition(7).color = "orange"
-// game.faceInPosition(1).blockInPosition(8).color = 'blue'
-// game.faceInPosition(2).blockInPosition(9).color = 'white'
+    // game.faceInPosition(1).blockInPosition(1).color = 'white'
+    // game.faceInPosition(1).blockInPosition(2).color = 'blue'
+    // game.faceInPosition(1).blockInPosition(3).color = "yellow"
+    // game.faceInPosition(1).blockInPosition(4).color = "green"
+    // game.faceInPosition(1).blockInPosition(5).color = "red"
+    // game.faceInPosition(1).blockInPosition(6).color = "orange"
+    // game.faceInPosition(1).blockInPosition(7).color = "orange"
+    // game.faceInPosition(1).blockInPosition(8).color = 'blue'
+    // game.faceInPosition(2).blockInPosition(9).color = 'white'
 
-// game.faceInPosition(2).blockInPosition(1).color = 'white'
-// game.faceInPosition(2).blockInPosition(2).color = 'blue'
-// game.faceInPosition(2).blockInPosition(3).color = "yellow"
-// game.faceInPosition(2).blockInPosition(4).color = "green"
-// game.faceInPosition(1).blockInPosition(5).color = "red"
-// game.faceInPosition(2).blockInPosition(6).color = "orange"
-// game.faceInPosition(2).blockInPosition(7).color = 'blue'
-// game.faceInPosition(2).blockInPosition(8).color = "white"
-// game.faceInPosition(2).blockInPosition(9).color = 'orange'
+    // game.faceInPosition(2).blockInPosition(1).color = 'white'
+    // game.faceInPosition(2).blockInPosition(2).color = 'blue'
+    // game.faceInPosition(2).blockInPosition(3).color = "yellow"
+    // game.faceInPosition(2).blockInPosition(4).color = "green"
+    // game.faceInPosition(1).blockInPosition(5).color = "red"
+    // game.faceInPosition(2).blockInPosition(6).color = "orange"
+    // game.faceInPosition(2).blockInPosition(7).color = 'blue'
+    // game.faceInPosition(2).blockInPosition(8).color = "white"
+    // game.faceInPosition(2).blockInPosition(9).color = 'orange'
 
-// game.faceInPosition(3).blockInPosition(1).color = 'white'
-// game.faceInPosition(3).blockInPosition(2).color = 'blue'
-// game.faceInPosition(3).blockInPosition(3).color = "yellow"
-// game.faceInPosition(3).blockInPosition(4).color = "green"
-// game.faceInPosition(1).blockInPosition(5).color = "red"
-// game.faceInPosition(3).blockInPosition(6).color = "orange"
-// game.faceInPosition(3).blockInPosition(7).color = 'white'
-// game.faceInPosition(3).blockInPosition(8).color = "orange"
-// game.faceInPosition(3).blockInPosition(9).color = 'blue'
+    // game.faceInPosition(3).blockInPosition(1).color = 'white'
+    // game.faceInPosition(3).blockInPosition(2).color = 'blue'
+    // game.faceInPosition(3).blockInPosition(3).color = "yellow"
+    // game.faceInPosition(3).blockInPosition(4).color = "green"
+    // game.faceInPosition(1).blockInPosition(5).color = "red"
+    // game.faceInPosition(3).blockInPosition(6).color = "orange"
+    // game.faceInPosition(3).blockInPosition(7).color = 'white'
+    // game.faceInPosition(3).blockInPosition(8).color = "orange"
+    // game.faceInPosition(3).blockInPosition(9).color = 'blue'
 
-// game.faceInPosition(4).blockInPosition(1).color = 'white'
-// game.faceInPosition(4).blockInPosition(2).color = 'blue'
-// game.faceInPosition(4).blockInPosition(3).color = "yellow"
-// game.faceInPosition(4).blockInPosition(4).color = "green"
-// game.faceInPosition(1).blockInPosition(5).color = "red"
-// game.faceInPosition(4).blockInPosition(6).color = "orange"
-// game.faceInPosition(4).blockInPosition(7).color = "red"
-// game.faceInPosition(4).blockInPosition(8).color = "yellow"
-// game.faceInPosition(4).blockInPosition(9).color = 'green'
+    // game.faceInPosition(4).blockInPosition(1).color = 'white'
+    // game.faceInPosition(4).blockInPosition(2).color = 'blue'
+    // game.faceInPosition(4).blockInPosition(3).color = "yellow"
+    // game.faceInPosition(4).blockInPosition(4).color = "green"
+    // game.faceInPosition(1).blockInPosition(5).color = "red"
+    // game.faceInPosition(4).blockInPosition(6).color = "orange"
+    // game.faceInPosition(4).blockInPosition(7).color = "red"
+    // game.faceInPosition(4).blockInPosition(8).color = "yellow"
+    // game.faceInPosition(4).blockInPosition(9).color = 'green'
 
-// game.faceInPosition(5).blockInPosition(1).color = 'white'
-// game.faceInPosition(5).blockInPosition(2).color = 'blue'
-// game.faceInPosition(5).blockInPosition(3).color = "yellow"
-// game.faceInPosition(5).blockInPosition(4).color = "green"
-// game.faceInPosition(1).blockInPosition(5).color = "red"
-// game.faceInPosition(5).blockInPosition(6).color = "orange"
-// game.faceInPosition(5).blockInPosition(7).color = 'green'
-// game.faceInPosition(5).blockInPosition(8).color = "red"
-// game.faceInPosition(5).blockInPosition(9).color = 'yellow'
+    // game.faceInPosition(5).blockInPosition(1).color = 'white'
+    // game.faceInPosition(5).blockInPosition(2).color = 'blue'
+    // game.faceInPosition(5).blockInPosition(3).color = "yellow"
+    // game.faceInPosition(5).blockInPosition(4).color = "green"
+    // game.faceInPosition(1).blockInPosition(5).color = "red"
+    // game.faceInPosition(5).blockInPosition(6).color = "orange"
+    // game.faceInPosition(5).blockInPosition(7).color = 'green'
+    // game.faceInPosition(5).blockInPosition(8).color = "red"
+    // game.faceInPosition(5).blockInPosition(9).color = 'yellow'
 
-// game.faceInPosition(6).blockInPosition(1).color = 'white'
-// game.faceInPosition(6).blockInPosition(2).color = 'blue'
-// game.faceInPosition(6).blockInPosition(3).color = "yellow"
-// game.faceInPosition(6).blockInPosition(4).color = "green"
-// game.faceInPosition(1).blockInPosition(5).color = "red"
-// game.faceInPosition(6).blockInPosition(6).color = "orange"
-// game.faceInPosition(6).blockInPosition(7).color = 'yellow'
-// game.faceInPosition(6).blockInPosition(8).color = "green"
-// game.faceInPosition(6).blockInPosition(9).color = 'red'
+    // game.faceInPosition(6).blockInPosition(1).color = 'white'
+    // game.faceInPosition(6).blockInPosition(2).color = 'blue'
+    // game.faceInPosition(6).blockInPosition(3).color = "yellow"
+    // game.faceInPosition(6).blockInPosition(4).color = "green"
+    // game.faceInPosition(1).blockInPosition(5).color = "red"
+    // game.faceInPosition(6).blockInPosition(6).color = "orange"
+    // game.faceInPosition(6).blockInPosition(7).color = 'yellow'
+    // game.faceInPosition(6).blockInPosition(8).color = "green"
+    // game.faceInPosition(6).blockInPosition(9).color = 'red'
 }
 
 updateColors();
